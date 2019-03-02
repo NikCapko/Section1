7283b725d7532d082e26d71cf0a26e8e73bc5b9a
NikCapko <nik.capko@mail.ru>
2019-03-02 23:09:22 +0300

    section1_task5 - fix sign up screen

--- app/src/main/java/com/example/section1/Person.java
+++ app/src/main/java/com/example/section1/Person.java
@@ -12,6 +12,14 @@ public class Person {
     public Person() {
     }
 
+    public Person(String firstName, String lastName, String email, String login, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.login = login;
+        this.password = password;
+    }
+
     public Person(int id, String firstName, String lastName, String email, String login, String password) {
         this.id = id;
         this.firstName = firstName;
