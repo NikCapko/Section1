52582c67837bccc60d5f0c5959baf98f37615e4e
NikCapko <nik.capko@mail.ru>
2019-04-12 16:01:41 +0300

    fix app exit dialog

--- app/src/main/java/com/example/section1/ui/categories/CategoriesActivity.java
+++ app/src/main/java/com/example/section1/ui/categories/CategoriesActivity.java
@@ -1,6 +1,5 @@
 package com.example.section1.ui.categories;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
