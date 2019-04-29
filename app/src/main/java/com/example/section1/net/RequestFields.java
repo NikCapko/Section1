3ba63fe36b9acc6bcfa293021548da2492c587a3
NikCapko <nik.capko@mail.ru>
2019-04-29 16:20:58 +0300

    fix url

--- app/src/main/java/com/example/section1/net/RequestFields.java
+++
@@ -1,8 +0,0 @@
-package com.example.section1.net;
-
-public class RequestFields {
-
-    public static final String CATEGORY_ID = "category_id";
-    public static final String PRODUCT_ID = "product_id";
-    public static final String BASKET_PRODUCT_ID = "id";
-}
--- app/src/main/java/com/example/section1/net/Urls.java
+++ app/src/main/java/com/example/section1/net/Urls.java
@@ -7,9 +7,4 @@ public class Urls {
     public static final String LOGIN = "/auth/login";
     public static final String REGISTRATION = "/auth/registration";
     public static final String GET_CATEGORIES = "/market/categories";
-    public static final String GET_PRODUCTS = "/market/categories/{category_id}/products";
-    public static final String GET_PRODUCT_DETAIL = "/market/categories/{category_id}/products/{product_id}";
-    public static final String BASKET_ADD_PRODUCT = "/market/basket/add";
-    public static final String BASKET_REMOVE_PRODUCT = "/market/basket/remove";
-    public static final String BASKET_CHECKOUT = "/market/basket/checkout";
 }
